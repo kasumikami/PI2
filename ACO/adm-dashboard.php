156078f9a5f71bd24f31d4bfe0eae31c7f2b3756
Kamilla <49867864+kasumikami@users.noreply.github.com>
2020-05-13 11:58:24 -0300

    Add files via upload

---
+++ ACO/adm-dashboard.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta content='IE=edge' http-equiv='X-UA-Compatible'/>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+    <meta content='' name='author'/>
+    <meta content='' name='description'/>
+    <meta content='' name='keywords'/>
+    <link href='css/bootstrap.min.css' media='all' rel='stylesheet' type='text/css'/>
+    <link href='css/font-awesome.min.css' media='all' rel='stylesheet' type='text/css'/>
+    <link href='css/SidebarNav.min.css' media='all' rel='stylesheet' type='text/css'/>
+    <script src="https://kit.fontawesome.com/bf4d0ad315.js" crossorigin="anonymous"></script>
+    <title>Dashboard</title>
+  </head>
+  <body>
+    <div class='wrapper'>
+      <aside class="sidebar-left navbar navbar-inverse">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".collapse" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">ACO</a>
+        </div>
+        <nav class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="sidebar-menu">
+            <li class="header">MENU</li>
+            <li><a href="adm-dashboard.php"><i class="fas fa-user"></i> <span>PERFIL</span></a></li>
+            <li><a href="#"><i class="fa fa-edit"></i> <span>Cadastrar Categoria</span></a></li>
+            <li><a href="#"><i class="fas fa-sign-out-alt"></i> <span>Sair</span></a></li>
+            
+          </ul>
+        </nav>
+      </aside>
+    </div>
+    
+    <script src='js/jquery.min.js' type='text/javascript'></script>
+    <script src='js/bootstrap.min.js' type='text/javascript'></script>
+    <script src='js/SidebarNav.min.js' type='text/javascript'></script>
+    <script src="js/validator.min.js"></script>
+    
+    <script>
+    $('.sidebar-menu').SidebarNav()
+    </script>
+  </body>
+</html>
+
+<footer>
+  <p></p>
+</footer>
